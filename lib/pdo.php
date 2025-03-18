e35dc37f93b38c8f85381fb6467d69516a89ebc9
Isac <isacanyamah@gmail.com>
2025-03-18 20:14:13 +0100

    Fix connexion base de données Heroku

--- lib/pdo.php
+++ lib/pdo.php
@@ -19,3 +19,18 @@ if ($url) {
 } else {
     die("❌ Erreur : Impossible de récupérer l'URL de la base de données.");
 }
+$url = getenv('JAWSDB_URL') ?: die("❌ Erreur : Impossible de récupérer l'URL de la base de données.");
+
+$dbparts = parse_url($url);
+
+$host = $dbparts['host'];
+$dbname = ltrim($dbparts['path'], '/');
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur PDO : " . $e->getMessage());
+}
